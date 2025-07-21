a36985330c1fbf536be6173e07095174903ae6e8
HourBooster-ux <aali35364@gmail.com>
2025-07-21 20:06:09 +0500

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
+const PORT = process.env.PORT || 8080;
+
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('OK');
-}).listen(8080, () => {
-  console.log('Keep-alive server running on port 8080');
+}).listen(PORT, () => {
+  console.log(`Keep-alive server running on port ${PORT}`);
 });
